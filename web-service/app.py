3aa4796314d8ddcbd2be07e6990aaff22ccfdd24
jumblesale <rob@robsollars.co.uk>
2017-02-01 08:16:40 +0000

    created basic app structure

---
+++ web-service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, make_response
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return json_response({'message': 'hello'})
+
+
+def json_response(obj, status=200):
+    return make_response((
+        json.dumps(obj), status, {'Content-Type': 'application/json'}
+    ))
+
+if __name__ == "__main__":
+    app.run()
